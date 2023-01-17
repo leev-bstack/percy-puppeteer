66f6a0df34e75ffcab0daf58ea5ac2a9bef37c6e
leev-bstack <93796347+leev-bstack@users.noreply.github.com>
2023-01-17 14:41:59 -0600

    Update index.d.ts

--- types/index.d.ts
+++ types/index.d.ts
@@ -2,7 +2,7 @@ import * as Puppeteer from 'puppeteer';
 import { SnapshotOptions } from '@percy/core';
 
 export default function percySnapshot(
-  page: Puppeteer.Page,
+  page: any,
   name: string,
   options?: SnapshotOptions
 ): Promise<void>;
